2eb32c5f6559977966208c320772a5cc72762d45
Arman-Ghazaryan <91345263+Arman-Ghazaryan@users.noreply.github.com>
2022-03-14 22:10:13 +0400

    Update verify.h

--- verify.h
+++ verify.h
@@ -8,5 +8,24 @@ class verify
 {
 public:
 	void verifySize(int pos, string mesg);
-	void verifyIter(list<int>::Iterator pos, string mesg);
+	void verifyIter(typename list<T>::Iterator pos, string mesg);
 };
+
+template<typename T>
+void verify<T>::verifySize(int cond, string mesg)
+{
+	if (cond)
+	{
+	}
+	else
+	{
+		cout << mesg;
+		exit(cond);
+	}
+}
+
+template<typename T>
+void verify<T>::verifyIter(typename list<T>::Iterator pos, string mesg)
+{
+
+}
